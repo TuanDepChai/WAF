05433ced542390735d3b6c2659d9490fe184bf37
pmt1506 <pmtien150603@gmail.com>
2025-05-25 02:33:42 +0700

    add navigate login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,40 @@
 // src/components/Header.tsx
-import { Box, Flex, Button, Image, useDisclosure } from "@chakra-ui/react";
-import AuthModal from "./AuthModal";
+import { Box, Flex, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const { open, onOpen, onClose } = useDisclosure();
+    const navigate = useNavigate();
 
-  return (
-    <Box
-      as="header"
-      position="sticky"
-      top="0"
-      zIndex="100"
-      bg="#0a1020"
-      borderBottom="1px solid #232b47"
-      boxShadow="sm"
-      px={0}
-      py={2}
-    >
-      <Flex
-        maxW="container.xl"
-        mx="auto"
-        align="center"
-        justify="space-between"
-        px={8}
-      >
-        <Flex align="center" gap={3}>
-          <Image src={logo} alt="Logo" boxSize="36px" />
-          <Box fontWeight="bold" color="#3b82f6" fontSize="xl">
-            F-Guard WAF
-          </Box>
-        </Flex>
-        <Button colorScheme="blue" size="md" className="text-black!" onClick={onOpen}>
-          Log in
-        </Button>
-      </Flex>
-    </Box>
-  );
+    return (
+        <Box
+            as="header"
+            position="sticky"
+            top="0"
+            zIndex="100"
+            bg="#0a1020"
+            borderBottom="1px solid #232b47"
+            boxShadow="sm"
+            px={0}
+            py={2}
+        >
+            <Flex
+                maxW="container.xl"
+                mx="auto"
+                align="center"
+                justify="space-between"
+                px={8}
+            >
+                <Flex align="center" gap={3}>
+                    <Box fontWeight="bold" color="#3b82f6" fontSize="xl">
+                        F-Guard WAF
+                    </Box>
+                </Flex>
+                <Button colorScheme="blue" size="md" className="text-black!" onClick={() => navigate("/login")}>
+                    Log in
+                </Button>
+            </Flex>
+        </Box>
+    );
 };
 
 export default Header;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,41 +1,91 @@
 import {
     Button,
+    Container,
     Field,
-    Fieldset,
     Input,
-    Stack,
+    VStack,
+    Heading,
+    TabsRoot,
+    TabsList,
+    TabsTrigger,
+    Flex,
 } from "@chakra-ui/react"
+import { useState } from "react";
 
 const Login = () => {
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("Form submitted");
     }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <Fieldset.Root size="lg" maxW="md">
-                <Stack>
-                    <Fieldset.Legend>Login</Fieldset.Legend>
-                </Stack>
+        <Container
+            minH="100vh"
+            maxW="100%"
+            bg="#0a1020"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Flex>
+                <VStack
+                    w="xl"
+                    maxW="md"
+                    bg="white"
+                    px={30}
+                    py={10}
+                    borderRadius="lg"
+                    boxShadow="xl"
+                    gap={6}
+                >
+                    <TabsRoot variant={"line"} defaultValue={"login"} width={"full"}>
+                        <TabsList>
+                            <TabsTrigger value="login">Login</TabsTrigger>
+                            <TabsTrigger value="register">Register</TabsTrigger>
+                        </TabsList>
+                    </TabsRoot>
+                    
+                    <Heading size="lg" color="blue.500">Login</Heading>
+                    <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                        <VStack gap={4} w="full">
+                            <Field.Root>
+                                <Field.Label>Email</Field.Label>
+                                <Input
+                                    name="email"
+                                    placeholder="Enter your email"
+                                    size="lg"
+                                    variant="outline"
+                                />
+                            </Field.Root>
 
-                <Fieldset.Content>
-                    <Field.Root>
-                        <Field.Label>Email</Field.Label>
-                        <Input name="email" />
-                    </Field.Root>
+                            <Field.Root>
+                                <Field.Label>Password</Field.Label>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    placeholder="Enter your password"
+                                    size="lg"
+                                    variant="outline"
+                                />
+                            </Field.Root>
 
-                    <Field.Root>
-                        <Field.Label>Password</Field.Label>
-                        <Input name="password" type="password" />
-                    </Field.Root>
-
-                </Fieldset.Content>
-
-                <Button type="submit" alignSelf="flex-start" className="w-100 bg-blue-500! text-black!">
-                    Submit
-                </Button>
-            </Fieldset.Root>
-        </form>
+                            <Button
+                                type="submit"
+                                variant={"solid"}
+                                bg={"blue.500"}
+                                color={"white"}
+                                size="lg"
+                                width="full"
+                                mt={4}
+                            >
+                                Sign In
+                            </Button>
+                        </VStack>
+                    </form>
+                </VStack>
+            </Flex>
+        </Container>
     )
 }
 
